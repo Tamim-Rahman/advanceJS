0704ae18f3a861c1bbc5e6a024be17ce64464231
Tamim-Rahman <ttamim.tr@gmail.com>
2020-03-25 22:50:46 +0600

    true Vs false

---
+++ true-false.js
@@ -0,0 +1,17 @@
+ // falsy:
+ // 0
+ //""
+ //undefined
+ //null
+ //NaN
+ //Truthy
+ // "0", " ", []
+
+ let name =12;
+ if(name || name== 0){
+     console.log("Condition is true");  
+ }
+ else{
+     console.log("Condition is false");
+     
+ }
